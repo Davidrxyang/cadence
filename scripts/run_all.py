42ce04e924a93ff880ea43a3d5776cc42b509077
davidrxyang <davidrxyang@hotmail.com>
2026-06-02 02:36:14 -0400

    update params for visualizer

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -15,8 +15,8 @@ def main():
         {"command": "python3 scripts/run_setup.py", "description": "Setting up the environment"},
         {"command": "python3 scripts/run_build.py", "description": "Building the simulator"},
         {"command": "python3 scripts/run_import_parrallel.py", "description": "Importing datasets"},
-        {"command": "python3 scripts/run_experiments_parrallel.py", "description": "Running experiments"},
-        {"command": "python3 scripts/run_plots.py", "description": "Generating plots"}
+        {"command": "python3 scripts/run_experiments_parrallel.py", "description": "Running experiments"}
+        #{"command": "python3 scripts/run_plots.py", "description": "Generating plots"}
     ]
 
     # Execute each command sequentially
